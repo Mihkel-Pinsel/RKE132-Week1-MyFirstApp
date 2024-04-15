54cfe75f7a52fa61ed39314e23f099b43d8d20ec
Mihkel Pinsel. <mihkel.pinsel@tktk.ee>
2024-04-15 14:33:10 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//rakendus küsib nime ja tervitab teda!
+
+
+using System.Transactions;
+
+Console.WriteLine("Enter your name:");
+//sõned
+string userName = Console.ReadLine();
+
+
+Console.WriteLine($"Hello, {userName}!");
